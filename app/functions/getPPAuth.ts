ca30691b2dac126c13fb670c8344e1a96e20608e
Kelvin Mitau <kelvinmitau05@gmail.com>
2025-03-02 15:28:07 +0300

    Fixing a bug

--- app/functions/getPPAuth.ts
+++ app/functions/getPPAuth.ts
@@ -1,8 +1,9 @@
+import config from "dotenv"
 
 export default async function(){
     const endpoint = "https://api-m.sandbox.paypal.com/v1/oauth2/token";
-        const clientId = "AajGhAoim_v_1lq9EvZcpcribRqHynGwDSouoGQF_UmhdGwSaWwaJk8iE_XFrqBhP4_ZaK8jDdzjBPkm";
-        const clientSecret = "EC_v4XiW7mUJdfVR-IWK8Wd28gpZmE2CfUMVZq4endHX-SYGa_XDay0NOlILmJgPckcUgkf5KD7Tkiy1";
+        const clientId = process.env.PPZ_CLIENT_ID;
+        const clientSecret = process.env.PP_SECRET;
         const credentials = btoa(`${clientId}:${clientSecret}`);
         const authTokenRequest = await fetch(endpoint, {
             method: "POST",
