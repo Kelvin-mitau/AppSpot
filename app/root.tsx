f41a3335b63c7dea3c78b97347e98eac925bdd6d
Kelvin Mitau <kelvinmitau05@gmail.com>
2025-03-10 08:51:47 +0300

    Fixing styles importation

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,8 @@ import {
 } from "@remix-run/react";
 import type { LinksFunction, MetaFunction } from "@remix-run/node";
 
+import "./tailwind.css";
+
 
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
